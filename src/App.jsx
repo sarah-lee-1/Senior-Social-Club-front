d956a52e81ce98ea0245726b2496520db32bdc9f
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2022-02-14 14:25:54 -0600

    Perf: Add View All Members function

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,35 @@ import React, { useState, useEffect } from 'react';
 function App() {
 
   const [events, setEvents] = useState([]);
+  conts [member, setMember] = useState([]);
   conts [members, setMembers] = useState([]);
   const [socialClubs, setSocialClubs] = useState([]);
 
   useEffect(() => {
-    viewAllEvents();
-    createMember(); 
+    viewAllMembers();
+    createMember();
+
+  
+     
   }, [])
+
+
+
+  async function viewAllMembers() {
+    let response = await axios.get('http://127.0.0.1:8000/api/members/view_all_members/');
+    setMembers(response.data)
+  }
 
   // create_member
   async function createMember(newMember) {
     let response = await axios.post('http://127.0.0.1:8000/api/members/create_member/'), newMember;
     if(response.status ===201) {
       console.log("New Member Created!")
-      await getAllMembers
+      await getAllMembers();
     }
   }
 
-  // view_all_members
+ 
   // membership_request
   // view_profile
   // update_profile
